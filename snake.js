e31870cb86fcdea6174edb4a4a38a92b8af89266
Garrett <gman9mm@live.com>
2021-03-30 16:37:35 -0700

    Fix directionals.

--- snake.js
+++ snake.js
@@ -7,11 +7,11 @@ function Snake() {
   this.dir = function (x, y) {
     this.xspeed = x;
     this.yspeed = y;
-  }
+  };
 
   this.update = function () {
-    this.x = this.x + this.xspeed + scl;
-    this.y = this.y + this.yspeed + scl;
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
 
     this.x = constrain(this.x, 0, width - scl);
     this.y = constrain(this.y, 0, height - scl);
