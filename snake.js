93860dcfd9c7d916959a982a0d4a5ef91903aee5
Garrett <gman9mm@live.com>
2021-03-30 17:02:33 -0700

    Tail now extends, but only once due to bug.

--- snake.js
+++ snake.js
@@ -3,10 +3,13 @@ function Snake() {
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
 
   this.eat = function (pos) {
     let d = dist(this.x, this.y, pos.x, pos.y);
     if (d < 1) {
+      this.total++;
       return true;
     } else {
       return false;
@@ -20,15 +23,24 @@ function Snake() {
 
 
   this.update = function () {
+    for (var i = 0; i < this.total - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
     this.x = this.x + this.xspeed * scl;
     this.y = this.y + this.yspeed * scl;
 
     this.x = constrain(this.x, 0, width - scl);
     this.y = constrain(this.y, 0, height - scl);
+
   }
 
   this.show = function () {
     fill(255);
+    for (var i = 0; i < this.total; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
     rect(this.x, this.y, scl, scl);
   }
 
